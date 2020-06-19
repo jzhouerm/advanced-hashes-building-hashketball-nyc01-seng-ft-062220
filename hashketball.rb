d0b51bb6ef6c7541056e8ca05f58c633a15f5d33
JULIA ZHOU <jzhouerm@hotmail.com>
2020-06-19 15:46:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,33 @@
-# Write your code here!
+def game_hash
+  hashketball = {
+  :home => {
+    :team_name => "Brooklyn Nets"
+    :colors =>["Black", "White"]
+    :players => [{
+      :player_name => "Alan Anderson"
+      :player_number => 0
+      :shoe => 16
+      :points => 22
+      :rebounds => 12
+      :assists => 12
+      :steals => 3
+      :blocks => 1
+      :slam_dunks => 1
+      }]
+
+  :away => {
+    :team_name =>"Charlotte Hornets"
+    :colors => ["Turquoise", "Purple"]
+    :players => [{
+      :player_name => "Jeff Adrien"
+      :player_number => 4
+      :shoe => 18
+      :points => 10
+      :rebounds => 1
+      :assists => 1
+      :steals => 2
+      :blocks => 7
+      :slam_dunks => 2
+      }]
+  }
+}
