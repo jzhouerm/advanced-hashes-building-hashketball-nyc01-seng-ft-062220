9c5c38e7afc9040f4d9078f0b463c8df47782557
JULIA ZHOU <jzhouerm@hotmail.com>
2020-06-19 16:39:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -88,14 +88,14 @@ def game_hash
     },
     {
       :player_name => "Desagna Diop",
-      :player_number => 0,
-      :shoe => 16,
-      :points => 12,
-      :rebounds => 4,
-      :assists => 7,
-      :steals => 22,
-      :blocks => 15,
-      :slam_dunks => 10
+      :player_number => 2,
+      :shoe => 14,
+      :points => 24,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 4,
+      :blocks => 5,
+      :slam_dunks => 5
     },
     {
       :player_name => "Ben Gordon",
