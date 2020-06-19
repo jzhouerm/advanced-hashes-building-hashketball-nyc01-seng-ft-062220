f58d4419e16aa34659d8c4d0c8997a862bde73ff
JULIA ZHOU <jzhouerm@hotmail.com>
2020-06-19 16:12:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -13,7 +13,52 @@ def game_hash
       :steals => 3,
       :blocks => 1,
       :slam_dunks => 1
-      }],
+    },
+      {
+        :player_name => "Reggie Evans",
+        :player_number => 0,
+        :shoe => 16,
+        :points => 12,
+        :rebounds => 4,
+        :assists => 7,
+        :steals => 22,
+        :blocks => 15,
+        :slam_dunks => 10
+      },
+      {
+        :player_name => "Brook Lopez",
+        :player_number => 0,
+        :shoe => 16,
+        :points => 12,
+        :rebounds => 4,
+        :assists => 7,
+        :steals => 22,
+        :blocks => 15,
+        :slam_dunks => 10
+      },
+      {
+        :player_name => "Mason Plumlee",
+        :player_number => 0,
+        :shoe => 16,
+        :points => 12,
+        :rebounds => 4,
+        :assists => 7,
+        :steals => 22,
+        :blocks => 15,
+        :slam_dunks => 10
+      },
+      {
+        :player_name => "Jason Terry",
+        :player_number => 0,
+        :shoe => 16,
+        :points => 12,
+        :rebounds => 4,
+        :assists => 7,
+        :steals => 22,
+        :blocks => 15,
+        :slam_dunks => 10 
+      },
+    ],
 
   :away => {
     :team_name =>"Charlotte Hornets",
@@ -28,6 +73,51 @@ def game_hash
       :steals => 2,
       :blocks => 7,
       :slam_dunks => 2
-      }]
+    },
+    {
+      :player_name => "Bismack Biyombo",
+      :player_number => 0,
+      :shoe => 16,
+      :points => 12,
+      :rebounds => 4,
+      :assists => 7,
+      :steals => 22,
+      :blocks => 15,
+      :slam_dunks => 10
+    },
+    {
+      :player_name => "Desagna Diop",
+      :player_number => 0,
+      :shoe => 16,
+      :points => 12,
+      :rebounds => 4,
+      :assists => 7,
+      :steals => 22,
+      :blocks => 15,
+      :slam_dunks => 10
+    },
+    {
+      :player_name => "Ben Gordon",
+      :player_number => 0,
+      :shoe => 16,
+      :points => 12,
+      :rebounds => 4,
+      :assists => 7,
+      :steals => 22,
+      :blocks => 15,
+      :slam_dunks => 10
+    },
+    {
+      :player_name => "Kemba Walker",
+      :player_number => 0,
+      :shoe => 16,
+      :points => 12,
+      :rebounds => 4,
+      :assists => 7,
+      :steals => 22,
+      :blocks => 15,
+      :slam_dunks => 10
+    },
+    ]
   }
 }
